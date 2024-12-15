8265c56ac29b467f4937228779534482f99ce510
emmaudeji-zbook <gogreneinfo@gmail.com>
2024-12-15 21:40:28 +0100

    correct appointmentAlias

--- app/(workspaces)/workspace/(no-dashboard)/edit/page.tsx
+++ app/(workspaces)/workspace/(no-dashboard)/edit/page.tsx
@@ -3,15 +3,15 @@ import { fetchSchedule } from '@/lib/server/schedules'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-const EditAppointmentsPage = async ({searchParams:{alias}}:{
-  searchParams:{alias?:string}
+const EditAppointmentsPage = async ({searchParams:{appointmentAlias}}:{
+  searchParams:{appointmentAlias?:string}
 }) => {
-  if(!alias) {
+  if(!appointmentAlias) {
     redirect('/create')
   } 
-  const {data,error } = await fetchSchedule(alias!)
+  const {data,error } = await fetchSchedule(appointmentAlias!)
   return (
-    <CreateAppointments appointment={{...data, createdBy:data?.createdBy?.id}} serverError={error} alias={alias}/>
+    <CreateAppointments appointment={{...data, createdBy:data?.createdBy?.id}} serverError={error} alias={appointmentAlias}/>
   )
 }
 export default EditAppointmentsPage
