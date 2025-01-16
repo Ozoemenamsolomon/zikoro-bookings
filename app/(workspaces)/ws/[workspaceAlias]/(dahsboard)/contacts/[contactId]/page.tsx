794d9a2569b5807abd809541b06e16bdfef3a761
emmaudeji-zbook <gogreneinfo@gmail.com>
2025-01-16 11:00:35 +0100

    reiewing before merging

--- app/(workspaces)/ws/[workspaceAlias]/(dahsboard)/contacts/[contactId]/page.tsx
+++ app/(workspaces)/ws/[workspaceAlias]/(dahsboard)/contacts/[contactId]/page.tsx
@@ -3,7 +3,6 @@ import ContactInfo from "@/components/workspace/contact/ContactInfo";
 import ContactSubLayout from "@/components/workspace/contact/ContactSubLayout";
 import { fetchContacts } from "@/lib/server/contacts";
 import { unstable_noStore } from "next/cache";
-import { redirect } from "next/navigation";
 
 const Contacts = async ({
   params: { contactId, workspaceAlias },
