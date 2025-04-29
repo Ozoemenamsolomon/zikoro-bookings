8bb2fb03824c543f48638675acc18319b0df2cfa
emmaudeji-zbook <gogreneinfo@gmail.com>
2025-04-29 03:39:57 +0100

    supabase realtime

--- hooks/services/useSupabaseRealtime.ts
+++ hooks/services/useSupabaseRealtime.ts
@@ -4,23 +4,34 @@
 import { createClient } from '@/utils/supabase/client';
 import { useEffect } from 'react';
 
-export function useSupabaseRealtime<T>(
-  table: string,
-  onChange: (payload: T) => void
+type Payload = {
+  eventType: 'INSERT' | 'UPDATE' | 'DELETE';
+  schema: 'public';
+  table: string;
+  record: any   // new data
+  old_record: any // old data (for updates/deletes)
+};
+type RealtimeType = {
+  table: string, 
+  onChange: (payload:Payload) => void,
+  filter?:{filter:string},
+}
+export function useSupabaseRealtime(
+ {table, onChange, filter}:RealtimeType
 ) {
     const supabase = createClient()
     useEffect(() => {
         const channel = supabase
         .channel(`realtime:${table}`)
         .on(
-            'postgres_changes',
-            { event: '*', schema: 'public', table },
-            (payload) => {
-            console.log('Realtime payload:', payload);
-            onChange(payload as any);
-            }
-        )
-        .subscribe();
+          'postgres_changes',
+          { event: '*', schema: 'public', table },
+          (payload) => {
+          console.log('Realtime payload:', payload);
+          onChange(payload as any);
+          }
+      )
+      .subscribe();
 
         return () => {
         supabase.removeChannel(channel);
